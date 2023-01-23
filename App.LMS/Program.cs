6386ecc61cdfb5c83d79b22f2beaf29887b97f4a
Samuel Morgan Anderson <andersonhenderson45@gmail.com>
2023-01-23 14:20:17 -0500

    Initial Commit

---
+++ App.LMS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace App.LMS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
