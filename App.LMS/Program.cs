e1945ce45bee000b9eb5c49e5ac0783dd38bd790
sam-morgan <andersonhenderson45@gmail.com>
2023-02-22 18:10:46 -0500

    Added node tree (dialogue tree)

--- App.LMS/Program.cs
+++ App.LMS/Program.cs
@@ -11,6 +11,37 @@ namespace App.LMS
 
             LMSService svc = new LMSService();
 
+            DialogueTree dt = new DialogueTree();
+            Node cn = dt.CurrentNode;
+
+            cn.createChild("Person options").createMultiChild("Add a person")
+                                            .createMultiChild("Update a person")
+                                            .createMultiChild("Delete a person")
+                                            .createMultiChild("Find a person")
+                                            .createMultiChild("List all people");
+
+            cn = dt.CurrentNode.Children[1]; // Update a person
+                                             // TODO: Add node getter
+
+            cn.createMultiChild("");
+
+
+            cn.createChild("Course options").createMultiChild("Add a course")
+                                            .createMultiChild("Update a course")
+                                            .createMultiChild("Delete a course")
+                                            .createMultiChild("Find a course")
+                                            .createMultiChild("List all courses");
+
+            Node selected = dt.RootNode;
+            while (true)
+            {
+                Console.Clear();
+                selected = dt.chooseChildFromMenu(selected, true);
+            }
+
+
+
+
             /*  
              *  NOTE: If option depth >0, always print back option.
              *  
@@ -45,6 +76,7 @@ namespace App.LMS
              */
 
 
+            /*
             Console.Clear();
             Console.WriteLine("1. People Options");
             Console.WriteLine("2. Course Options");
@@ -63,10 +95,23 @@ namespace App.LMS
 
                 switch (select)
                 {
+                    // Add a person
                     case 1:
                         {
-                            string name;
-                            string 
+                            // Choosing fields for the person
+                            Console.Write("Name: ");
+                            var name = Console.ReadLine();
+                            Console.Write("Class Role: ");
+                            if (Enum.TryParse<ClassRoles>(Console.ReadLine(), true, out ClassRoles roleCnvt))
+                            {
+                                // Adding the person to the master list
+                                svc.AddPerson(name, roleCnvt);
+                                Console.WriteLine($"{svc.People.Last()}, has been created.");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Role {roleCnvt} not found");
+                            }
                         }
                         break;
                     case 2:
@@ -90,6 +135,258 @@ namespace App.LMS
             {
 
             }
+            */
+
+
+
+
+
+            /*
+            switch (x)
+            {
+                // OPTION 1: Add course
+                case 1:
+                    {
+                    }
+                    break;
+
+
+                // OPTION 2: Add person
+                case 2:
+                    {
+                        // Choosing fields for the person
+                        Console.Write("Name: ");
+                        var name = Console.ReadLine();
+                        Console.Write("Class Role: ");
+                        if (Enum.TryParse(Console.ReadLine(), out ClassRoles roleCnvt))
+                        {
+                            // Adding the person to the master list
+                            svc.AddPerson(name, roleCnvt);
+                            Console.WriteLine($"{svc.People.Last()}, has been created.");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Role {roleCnvt} not found");
+                        }
+                    }
+                    break;
+
+
+                // OPTION 3: Add person to a course
+                case 3:
+                    {
+                        // Picking the person
+                        Console.WriteLine("Which student do you want to add a course to?:");
+                        Person person = ChooseFromMenuList(svc.People);
+
+                        // Finding svc.Courses person isn't in
+                        List<Course> filteredCourses = svc.Courses
+                            .Where(c => !c.Roster.Contains(person)).ToList();
+
+                        if (filteredCourses.Count() > 0)
+                        {
+                            Console.WriteLine("Which course would you like to add?:");
+                            Course course = ChooseFromMenuList(filteredCourses);
+
+                            // Adding the person
+                            course.AddPerson(person);
+                            Console.WriteLine($"You have chosen to enroll {person} to {course}.");
+                        }
+                        else { Console.WriteLine("No availiable svc.Courses."); }
+                    }
+                    break;
+
+
+                // OPTION 4: Remove a person from a course
+                case 4:
+                    {
+                        // Selecting the course
+                        Console.WriteLine("Select the course to remove a person from:");
+                        Course course = ChooseFromMenuList(svc.Courses);
+                        Console.WriteLine($"You have chosen {course}");
+
+                        // Selecting the person
+                        Console.WriteLine($"Now, which student would you like to remove?:");
+                        Person person = ChooseFromMenuList(course.Roster);
+
+                        // Removing the person from the course
+                        Console.WriteLine($"You have chosen to remove {person} from {course}");
+                        course.RemovePerson(person);
+                    }
+
+                    break;
+
+
+                // OPTION 5: List all svc.Courses
+                case 5:
+                    // Simple function call
+                    PrintCoursesDetailed(svc.Courses);
+                    break;
+
+
+                // OPTION 6: Search for a course
+                case 6:
+                    {
+                        // Entering search term
+                        Console.Write("Please enter a course search term: ");
+                        var searchTerm = Console.ReadLine();
+
+                        // Searching for all svc.Courses that contain 'searchTerm'
+                        IEnumerable<Course> foundCourses = svc.Courses
+                            .Where(c => c.Name.Contains(searchTerm, StringComparison.InvariantCultureIgnoreCase) ||
+                                        c.Description.Contains(searchTerm, StringComparison.InvariantCultureIgnoreCase) ||
+                                        c.Code.Contains(searchTerm, StringComparison.InvariantCultureIgnoreCase));
+
+                        List<Course> result = foundCourses.ToList(); // Converting IEnumerable to List
+
+                        // Case for 0 results
+                        if (result.Count() == 0)
+                        { Console.WriteLine("No results found."); break; }
+
+                        // Ultimate output
+                        Console.WriteLine($"--> {result.Count()} results found!");
+                        PrintCoursesDetailed(result);
+                    }
+                    break;
+
+
+                // OPTION 7: List all svc.People
+                case 7:
+                    // Simple function call
+                    PrintListSelectionInterface(svc.People);
+                    break;
+
+
+                // OPTION 8: Search for a person
+                case 8:
+                    {
+                        // Entering search term
+                        Console.Write("Please enter a course search term: ");
+                        var searchTerm = Console.ReadLine();
+
+                        // Searching for all svc.People that contain 'searchTerm'
+                        IEnumerable<Person> foundPeople = svc.People
+                            .Where(p => p.Name.Contains(searchTerm, StringComparison.InvariantCultureIgnoreCase) ||
+                                        p.Role.ToString().Contains(searchTerm, StringComparison.InvariantCultureIgnoreCase));
+
+                        List<Person> result = foundPeople.ToList(); // Converting IEnumerable to List
+
+                        // Case for 0 results
+                        if (result.Count() == 0)
+                        { Console.WriteLine("No results found."); break; }
+
+                        // Ultimate output
+                        Console.WriteLine($"--> {result.Count()} results found!");
+                        PrintListSelectionInterface(result);
+                    }
+                    break;
+
+
+                // OPTION 9: List a person's svc.Courses
+                case 9:
+                    {
+                        Console.WriteLine("Which student's svc.Courses would you like to view?:");
+                        Person person = ChooseFromMenuList(svc.People);
+
+                        foreach (Course c in svc.Courses)
+                        {
+                            if (c.Roster.Contains(person))
+                            {
+                                Console.WriteLine($"{svc.Courses.IndexOf(c) + 1}.\t{c}");
+                            }
+
+                        }
+                    }
+                    break;
+
+
+                // OPTION 10: Update a course
+                case 10:
+                    {
+                        // Selecting the course
+                        Console.Write("Which course to update?: ");
+                        Course course = ChooseFromMenuList(svc.Courses);
+                        Console.WriteLine($"You have chosen to update {course}.");
+
+                        // Course's fields
+                        Console.Write("Course name: ");
+                        var name = Console.ReadLine();
+                        Console.Write("Class code: ");
+                        var code = Console.ReadLine();
+                        Console.Write("Description: ");
+                        var desc = Console.ReadLine();
+
+                        // Updating the course
+                        svc.UpdateCourse(course, name, code, desc);
+                        Console.WriteLine($"Course '{course}' has been update.");
+                    }
+                    break;
+
+
+                // OPTION 11: Update a person
+                case 11:
+                    {
+                        // Selecting the person
+                        Console.WriteLine("Which person to update?: ");
+                        Person person = ChooseFromMenuList(svc.People);
+                        Console.WriteLine($"You have chosen to update {person}.");
+
+                        // Person's fields
+                        Console.Write("Name: ");
+                        var name = Console.ReadLine();
+                        Console.Write("Class Role: ");
+                        if (Enum.TryParse(Console.ReadLine(), out ClassRoles roleCnvt))
+                        {
+                            // Updating the person
+                            svc.UpdatePerson(person, name, roleCnvt);
+                            Console.WriteLine($"{person} has been updated.");
+                        }
+                    }
+                    break;
+
+
+                // OPTION 12: Create an assignment and add to a course
+                case 12:
+                    {
+                        Console.Write("Assignment name: ");
+                        string name = Console.ReadLine() ?? string.Empty;
+                        Console.Write("Description: ");
+                        string desc = Console.ReadLine() ?? string.Empty;
+
+                        string ptsStr;
+                        uint pts;
+                        do
+                        {
+                            Console.Write("Total points: ");
+                            ptsStr = Console.ReadLine() ?? "0";
+                        } while (!uint.TryParse(ptsStr, out pts));
+
+                        string dueStr;
+                        DateTime dueDate;
+                        do
+                        {
+                            Console.Write("Due date [dd/mm/yyyy hh:mm {am or pm}]:\n\t");
+                            dueStr = Console.ReadLine() ?? DateTime.Today.ToString();
+                        } while (!DateTime.TryParse(dueStr, out dueDate));
+
+                        Assignment assignment = new Assignment(name, desc, pts, dueDate);
+
+                        Console.WriteLine($"Finally, which course would you like to add '{name}' to?:");
+                        Course course = ChooseFromMenuList(svc.Courses);
+
+                        course.AddAssignment(assignment);
+                        Console.WriteLine($"Assignment {assignment} has been added to {course}");
+
+                    }
+                    break;
+
+
+                // OPTION 13: Exit
+                case 13:
+                    cont = false;
+                    break
+            }
+            */
         }
 
         // ===================
@@ -171,3 +468,4 @@ namespace App.LMS
         }
     }
 }
+
---
+++ Library.LMS/Models/DialogueTree.cs
@@ -0,0 +1,95 @@
+﻿using System;
+namespace Library.LMS.Models
+{
+	public class Node
+	{
+		public Node? Parent { get; }
+		public List<Node> Children { get; }
+		public string? Data { get; set; }
+
+
+		// creating a root node
+		public Node()
+		{
+			Parent = null;
+			Data = null;
+			Children = new List<Node>();
+		}
+
+		// private constructor for children
+		private Node(Node p, string d)
+        {
+            Children = new List<Node>();
+			Data = d;
+        }
+
+		// methods
+		public Node createChild(string d)
+		{
+			Node child = new Node(this, d);
+			Children.Add(child);
+			return child;
+		}
+
+		public Node createMultiChild(string d)
+		{
+			Node child = new Node(this, d);
+			Children.Add(child);
+			return this;
+		}
+	}
+
+	public class DialogueTree
+	{
+		public Node RootNode { get; set; }
+		public Node CurrentNode { get; set; }
+
+		public DialogueTree()
+		{
+            RootNode = new Node();
+            CurrentNode = RootNode;
+		}
+
+        public void displayChildren(Node? p = null, bool numbered=false)
+		{
+			if (p == null) { p = CurrentNode; }
+
+			List<Node> children = p.Children;
+			foreach (Node n in children)
+			{
+				Console.Write((numbered) ? (children.IndexOf(n)+1) + ".\t" : "");
+				Console.WriteLine(n.Data);
+			}
+		}
+
+        public Node chooseChildFromMenu(Node? p = null, bool inclParent = false)
+        {
+			if (p.Parent == null) { inclParent = false; }
+			var length = p.Children.Count() + ((inclParent) ? 1 : 0);
+            if (p == null) { p = CurrentNode; }
+
+            displayChildren(p, true);
+			if (inclParent) { Console.WriteLine(length + ".\tBack"); }
+			while (true)
+			{
+				Console.Write(">> ");
+				if (int.TryParse(Console.ReadLine(), out int select))
+				{
+					--select;
+					if (select < length && select >= 0)
+                    {
+						if (select == length) { return p.Parent; }
+                        return p.Children[select];
+                    }
+				}
+			}
+		}
+
+		public Node goUp()
+		{
+            CurrentNode = CurrentNode.Parent ?? CurrentNode;
+			return CurrentNode;
+		}
+	}
+}
+
